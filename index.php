f03340e91758291d724a418f6dd8888329b7fc29
omthapa779 <omthapa781@gmail.com>
2022-03-30 09:49:21 +0545

    through php storm

---
+++ index.php
@@ -0,0 +1,18 @@
+<html lang="en">
+
+<head>
+    <title>Fabulor</title>
+    <link rel="stylesheet" type="text/css" href="css.css">
+</head>
+
+<body>
+ <div class="welcome_text">
+     <div class="welcome_overlay">
+     <h1 class="header">Welcome to FabuloR</h1>
+         <hr class="header_hr">
+         <div class="login_redirect"><a href="login.php">Login</a></div>
+         <div class="signup_redirect"><a href="signup.php">SignUP!</a></div>
+     </div>
+ </div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,18 @@
+<html lang="en">
+   <head>
+       <title>Login</title>
+       <link rel="stylesheet" type="text/css" href="css.css">
+   </head>
+<body>
+<div class="login_main">
+ <div class="login_background">
+    <h1 class="login_text">Login</h1>
+     <form method="post">
+         <div class="email_border"><input type="email" placeholder="Enter Email"></div>
+         <div class="password_border"><input type="password" placeholder="Enter Password"></div>
+          <div class="login_submit"><button type="submit">Submit</button></div>
+     </form>
+ </div>
+</div>
+</body>
+</html>
