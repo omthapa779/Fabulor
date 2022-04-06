8c4dc40670eacaae2de5336e7c3f00f94fe2809d
omthapa779 <omthapa781@gmail.com>
2022-04-06 12:23:17 +0545

    Signup site created

--- signup.php
+++ signup.php
@@ -1,11 +1,28 @@
-<html>
+<html lang="en">
 <head>
     <title>Fabulor | SignUP</title>
     <link type="text/css" rel="stylesheet" href="css.css">
 </head>
 <body>
   <div class="welcome_text">
-
+   <div class="back_button">
+       <a href="index.php">
+       <svg xmlns="http://www.w3.org/2000/svg" width="40" height="50" fill="#C3073F" class="bi bi-arrow-left-square-fill" viewBox="0 0 16 16">
+           <path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1z"/>
+       </svg>
+       </a>
+   </div>
+      <div class="sign_box">
+         <h1>Sign UP</h1>
+          <div class="username">USERNAME : </div>
+              <input type="text" placeholder="Create Username" id="input_username">
+          <div class="password">PASSWORD : </div>
+          <input type="text" placeholder="Create Password" id="input_password">
+          <div class="signup_thing">
+              <button id="register">CREATE</button>
+          </div>
+      </div>
+  </div>
   </div>
 </body>
 </html>
